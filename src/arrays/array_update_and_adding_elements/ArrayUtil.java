78a0402b2de4ec285ce6faf33f27a752a6b9d793
Alash95 <oyinlola@eksusmartschool.com.ng>
2023-05-30 10:12:26 +0100

    Arrays implementation

---
+++ src/arrays/array_update_and_adding_elements/ArrayUtil.java
@@ -0,0 +1,34 @@
+package array_update_and_adding_elements;
+
+public class ArrayUtil {
+
+    public static void printArray(int[] arr) {
+        int size = arr.length;
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void arrayDemo() {
+        int[] myArray = new int[5];
+//        printArray(myArray);
+        myArray[0] = 5;
+        myArray[1] = 1;
+        myArray[2] = 8;
+        myArray[3] = 2;
+        myArray[4] = 10;
+        myArray[2] = 9;
+        printArray(myArray);
+        System.out.println(myArray.length);
+        System.out.println(myArray[myArray.length - 1]);
+
+        int[] arr = {5,1,8,10};
+        printArray(arr);
+    }
+
+    public static void main(String[] args) {
+        arrayDemo();
+    }
+}
+
---
+++ src/arrays/natural_numbers_sum/SumOfNaturalNumbers.java
@@ -0,0 +1,25 @@
+package natural_numbers_sum;
+
+public class SumOfNaturalNumbers {
+
+    public static void main(String[] args) {
+        double now = System.currentTimeMillis();
+
+        System.out.println(sumOfNNumbers(99999));
+
+        System.out.println("Time taken - " + (System.currentTimeMillis() - now) + "millisecs.");
+
+    }
+
+//    public static int sumOfNNumbers(int n) {
+//        return n * (n + 1) /2;
+//    }
+
+    public static int sumOfNNumbers(int n) {
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+}
